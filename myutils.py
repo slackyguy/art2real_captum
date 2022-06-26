2c462c731453df11bbe398fe468b9066fc3e9e6e
Edinaldo Santos <edinaldo.santos@siemens-energy.com>
2022-06-26 16:02:46 -0300

    .

--- myutils.py
+++ myutils.py
@@ -19,9 +19,13 @@ class CustomClassifierOutputTarget:
         return model_output[:, self.category]
 
 def tensor_to_image(tensor):
-    tensor = tensor*255
-    tensor = np.array(tensor.cpu().detach().numpy(), dtype=np.uint8)
-    if np.ndim(tensor)>3:
-        assert tensor.shape[0] == 1
-        tensor = tensor[0]
-    return Image.fromarray(tensor)
+    arr = np.ndarray(tensor)#This is your tensor
+    arr_ = np.squeeze(arr) # you can give axis attribute if you wanna squeeze in specific dimension
+    #plt.imshow(arr_)
+    return Image.fromarray(arr_)
+    # tensor = tensor*255
+    # tensor = np.array(tensor.cpu().detach().numpy(), dtype=np.uint8)
+    # if np.ndim(tensor)>3:
+    #     assert tensor.shape[0] == 1
+    #     tensor = tensor[0]
+    # return Image.fromarray(tensor)
