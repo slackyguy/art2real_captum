99004dc24bb3667ed9ba1c02364a45bf505a690d
Edinaldo Santos <edinaldo.santos@siemens-energy.com>
2022-06-26 15:35:49 -0300

    .

--- rungradcam2.py
+++ rungradcam2.py
@@ -82,7 +82,10 @@ if __name__ == '__main__':
     #target_layers = resnet.model
     #target_layers = resnet.model.layers[-1]
     target_layers = torch.nn.Sequential(*list(resnet.children())[:-1])
-    print(len(*list(resnet.children())))
+
+    resnet_layers = list(resnet.children())
+    for layer in resnet_layers:
+        print(layer)
 
     pil_img = Image.open(list(dataset)[sample_index]['A_paths'][0])
 
