adcd279b6eb9e632be65751ba2cf23f88d570954
Edinaldo Santos <edinaldo.santos@siemens-energy.com>
2022-06-26 16:13:54 -0300

    .

--- myutils.py
+++ myutils.py
@@ -22,7 +22,8 @@ def tensor_to_image(tensor):
     #arr = np.ndarray(tensor)#This is your tensor
     #arr_ = np.squeeze(arr) # you can give axis attribute if you wanna squeeze in specific dimension
     #plt.imshow(arr_)
-    return tensor.detach().cpu().numpy()[0] #Image.fromarray(arr_)
+    img_data = tensor.detach().cpu().numpy()[0] #Image.fromarray(arr_)
+    return Image.fromarray(img_data)
     # tensor = tensor*255
     # tensor = np.array(tensor.cpu().detach().numpy(), dtype=np.uint8)
     # if np.ndim(tensor)>3:
