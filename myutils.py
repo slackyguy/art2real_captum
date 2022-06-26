361c6d0a093befb530034f3adaa6516cabbda633
Edinaldo Santos <edinaldo.santos@siemens-energy.com>
2022-06-26 13:35:06 -0300

    .

--- myutils.py
+++ myutils.py
@@ -6,7 +6,7 @@ class CustomClassifierOutputTarget:
     def __init__(self, category):
         self.category = category
     def __call__(self, model_output):
-        print(model_output)
+        print(model_output.shape)
         if len(model_output.shape) == 1:
             return model_output[self.category]
         return model_output[:, self.category]
